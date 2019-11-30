ba7c15129f328f1ebf556230b41cfffc0b939aa0
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-30 18:06:33 +0000

    Introduce an interface

--- src/com/luv2code/springdemo/BaseballCoach.java
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -1,6 +1,6 @@
 package com.luv2code.springdemo;
 
-public class BaseballCoach {
+public class BaseballCoach implements Coach{
     public String getDailyWorkout() {
         return "Spend 30 min on batting practice";
     }
--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -3,9 +3,9 @@ package com.luv2code.springdemo;
 public class MyApp {
     public static void main(String[] args) {
         // create the object
-        BaseballCoach theCoach = new BaseballCoach();
+        Coach theCoach = new BaseballCoach();
+
         // use the object
         System.out.println(theCoach.getDailyWorkout());
-
     }
 }
